984cd1045f5673dd60ef100d830adc9972b47833
MohammedNimer <mohammed_nimer_98@yahoo.com>
2023-07-02 23:23:52 +0300

    Yaml Update 1

--- src/test/java/data/Errors.java
+++ src/test/java/data/Errors.java
@@ -2,6 +2,7 @@ package data;
 
 public class Errors {
 
+
 	public static final String EMAIL_ALREADY_REGISTERED ="Email is already exists in the Database";
 	public static final String INCORRECT_PASSWORD ="Please Fill a correct Password";
 	public static final String ITEM_LENGTH_MUST_BE_MORE_THAN_3_CHARACTERS="\"item\" length must be at least 3 characters long";
