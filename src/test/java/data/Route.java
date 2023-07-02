f2b08988300459b23b9a97ce8136462d83c03661
MohammedNimer <mohammed_nimer_98@yahoo.com>
2023-07-02 22:31:10 +0300

    update yaml file

--- src/test/java/data/Route.java
+++ src/test/java/data/Route.java
@@ -7,7 +7,6 @@ public class Route {
 	public static final String REGISTER_ROUTE = "api/v1/users/register";
 	public static final String LOGIN_ROUTE = "api/v1/users/login";
 	public static final String TASKS_ROUTE = "api/v1/tasks/";
-	public static final String regsterPath = "";
 
 	
 }
